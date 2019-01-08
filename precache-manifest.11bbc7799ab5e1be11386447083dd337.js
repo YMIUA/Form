1b38c5946d6589ee1037353c51df8a40ba73fdfc
Mykola Yatsuk <ymiua17@gmail.com>
2019-01-08 16:21:32 +0200

    Updates

---
+++ precache-manifest.11bbc7799ab5e1be11386447083dd337.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba3540085227e18609af43ba762d7732",
+    "url": "/Form/static/media/cat2.ba354008.jpg"
+  },
+  {
+    "revision": "4081cd2ea61e6e760027",
+    "url": "/Form/static/css/main.483e2319.chunk.css"
+  },
+  {
+    "revision": "53ea4b1c6adefede9645",
+    "url": "/Form/static/js/1.53ea4b1c.chunk.js"
+  },
+  {
+    "revision": "86646779f0b22653a119",
+    "url": "/Form/static/js/runtime~main.86646779.js"
+  },
+  {
+    "revision": "1bf0ea9ad9527492f07e96c7ae2d894c",
+    "url": "/Form/static/media/cat1.1bf0ea9a.jpg"
+  },
+  {
+    "revision": "4081cd2ea61e6e760027",
+    "url": "/Form/static/js/main.4081cd2e.chunk.js"
+  },
+  {
+    "revision": "dcdd9149e72bee0b19d4a6a2debb70d1",
+    "url": "/Form/static/media/cat3.dcdd9149.jpg"
+  },
+  {
+    "revision": "2ce9e417a577bd34321bfc3d8788f258",
+    "url": "/Form/static/media/dog4.2ce9e417.jpg"
+  },
+  {
+    "revision": "87050fc6073c0e6944a4d962d1f38535",
+    "url": "/Form/static/media/angle-left.87050fc6.svg"
+  },
+  {
+    "revision": "f6b0c4c4f6360767be83de0177a33e1b",
+    "url": "/Form/static/media/angle-right.f6b0c4c4.svg"
+  },
+  {
+    "revision": "38351a5ef27ccd39b9790026324ddf08",
+    "url": "/Form/index.html"
+  }
+];
